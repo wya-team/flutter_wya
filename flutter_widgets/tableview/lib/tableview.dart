22f355be18e60c8629c7bd0541541098d5e1a7d1
李世航 <1228506851@qq.com>
2020-03-24 17:20:18 +0800

    添加tableview和noticebar组件

---
+++ flutter_widgets/tableview/lib/tableview.dart
@@ -0,0 +1,165 @@
+library tableview;
+
+import 'package:flutter/material.dart';
+
+typedef SectionHeaderView = Widget Function(BuildContext context, int index);
+typedef SectionFooterView = Widget Function(BuildContext context, int index);
+typedef NumberRowOfSection = int Function(BuildContext context, int index);
+typedef RowView = Widget Function(BuildContext context, int section, int row);
+
+//class TableView extends StatefulWidget {
+//  TableView({
+//    Key key,
+//    this.tableHeaderView,
+//    this.tableFooterView,
+//    @required this.sectionNumber,
+//    @required this.numberRowOfSection,
+//    this.sectionHeaderView,
+//    this.sectionFooterView,
+//    @required this.rowView,
+//      }) : super(key : key);
+//
+//  final Widget tableHeaderView;
+//  final Widget tableFooterView;
+//  SectionHeaderView sectionHeaderView;
+//  SectionFooterView sectionFooterView;
+//  final int sectionNumber;
+//  NumberRowOfSection numberRowOfSection;
+//  RowView rowView;
+//  @override
+//  _TableViewState createState() => _TableViewState();
+//}
+//
+//class _TableViewState extends State<TableView> {
+//  List<Widget> allWidget = [];
+//
+//
+//  void rowWidget(BuildContext context, int section) {
+//    List<Widget> alls = [];
+//    if (widget.tableHeaderView != null) {
+//      alls.add(widget.tableHeaderView);
+//    }
+//
+//    for(var s = 0; s<section; s++){
+//      List<Widget> rows = [];
+//      if (widget.sectionHeaderView != null){
+//        rows.add(widget.sectionHeaderView(context, s));
+//      }
+//
+//      var index = widget.numberRowOfSection(context, s);
+//      for(var i = 0; i<index;i++){
+//        rows.add(widget.rowView(context, s, i));
+//      }
+//      if (widget.sectionFooterView != null) {
+//        rows.add(widget.sectionFooterView(context, s));
+//      }
+//      alls.addAll(rows);
+//    }
+//
+//    if (widget.tableFooterView != null){
+//      alls.add(widget.tableFooterView);
+//    }
+//
+//    setState(() {
+//      allWidget = alls;
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    rowWidget(context, widget.sectionNumber);
+//    return ListView(
+//      children: allWidget,
+//    );
+//  }
+//}
+
+class TableView extends StatefulWidget {
+  TableView({
+    Key key,
+    this.tableHeaderView,
+    this.tableFooterView,
+    @required this.sectionNumber,
+    @required this.numberRowOfSection,
+    this.sectionHeaderView,
+    this.sectionFooterView,
+    @required this.rowView,
+  }) : super(key: key);
+
+  final Widget tableHeaderView;
+  final Widget tableFooterView;
+  final int sectionNumber;
+  SectionHeaderView sectionHeaderView;
+  SectionFooterView sectionFooterView;
+  NumberRowOfSection numberRowOfSection;
+  RowView rowView;
+  @override
+  _TableViewState createState() => _TableViewState();
+}
+
+class _TableViewState extends State<TableView> {
+  int allCount = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkAllCount();
+  }
+
+  @override
+  void didUpdateWidget(TableView oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    checkAllCount();
+  }
+
+  void checkAllCount(){
+    allCount = 0;
+    if (widget.tableHeaderView != null) {
+      allCount += 1;
+    }
+    allCount += widget.sectionNumber;
+    if (widget.tableFooterView != null) {
+      allCount += 1;
+    }
+  }
+
+  Widget sectionView(BuildContext context, int section) {
+    List<Widget> rows = [];
+    if (widget.sectionHeaderView != null) {
+      rows.add(widget.sectionHeaderView(context, section));
+    }
+
+    var index = widget.numberRowOfSection(context, section);
+    for (var i = 0; i < index; i++) {
+      rows.add(widget.rowView(context, section, i));
+    }
+    if (widget.sectionFooterView != null) {
+      rows.add(widget.sectionFooterView(context, section));
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: rows,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        if (widget.tableHeaderView != null && index == 0) {
+          return widget.tableHeaderView;
+        }
+        if (widget.tableHeaderView != null &&
+            widget.tableFooterView != null &&
+            index == allCount - 1) {
+          return widget.tableFooterView;
+        }
+        return sectionView(
+            context, widget.tableHeaderView != null ? index - 1 : index);
+      },
+      itemCount: allCount,
+    );
+  }
+}
