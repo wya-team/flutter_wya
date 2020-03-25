96be00cea38f1f16541c2052492240c22ca22984
李世航 <1228506851@qq.com>
2020-03-25 11:21:14 +0800

    添加圆形的CheckBox

---
+++ flutter_widgets/round_checkbox/lib/round_checkbox.dart
@@ -0,0 +1,42 @@
+library round_checkbox;
+
+import 'package:flutter/material.dart';
+
+class RoundCheckBox extends StatefulWidget {
+  var value = false;
+
+  Function(bool) onChanged;
+
+  RoundCheckBox({Key key, @required this.value, this.onChanged})
+      : super(key: key);
+
+  @override
+  _RoundCheckBoxState createState() => _RoundCheckBoxState();
+}
+
+class _RoundCheckBoxState extends State<RoundCheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+          onTap: () {
+            widget.value = !widget.value;
+            widget.onChanged(widget.value);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: widget.value
+                ? Icon(
+              Icons.check_circle,
+              size: 20.0,
+              color: Colors.lightBlue,
+            )
+                : Icon(
+              Icons.panorama_fish_eye,
+              size: 20.0,
+              color: Colors.grey,
+            ),
+          )),
+    );
+  }
+}
