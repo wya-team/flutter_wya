21bd84a3d387cf051cf6649c15ee0736dd1e8e59
李世航 <1228506851@qq.com>
2020-03-30 17:46:53 +0800

    添加noticebar自动滚动事件，添加md文件

--- flutter_widgets/noticebar/README.md
+++ flutter_widgets/noticebar/README.md
@@ -1,14 +1,13 @@
 # noticebar
 
-A new Flutter package.
+> 自左至右滚动的通知栏
 
-## Getting Started
-
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+```
+NoticeBar({
+    this.leftWidget, // 左边的组件
+    this.rightWidget, // 右边的组件
+    @required this.text,
+    this.type = NoticeType.constant, // 动画方式
+    this.backgroundColor = Colors.white,  // 背景颜色
+  }) : super();
+```
--- flutter_widgets/noticebar/lib/noticebar.dart
+++ flutter_widgets/noticebar/lib/noticebar.dart
@@ -1,18 +1,29 @@
 library noticebar;
+import 'dart:ui';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+
+enum NoticeType {
+  /// 连续性
+  constant,
+  /// 阶段性
+  subsection,
+}
 
 // ignore: must_be_immutable
 class NoticeBar extends StatefulWidget {
   NoticeBar({
-    this.leftImageName,
-    this.rightImageName,
-    this.text,
+    this.leftWidget,
+    this.rightWidget,
+    @required this.text,
+    this.type = NoticeType.constant,
     this.backgroundColor = Colors.white,
   }) : super();
-  String leftImageName;
-  String rightImageName;
+  Widget leftWidget;
+  Widget rightWidget;
   String text;
+  NoticeType type;
   Color backgroundColor;
   @override
   _NoticeBarState createState() => _NoticeBarState();
@@ -20,20 +31,26 @@ class NoticeBar extends StatefulWidget {
 
 class _NoticeBarState extends State<NoticeBar>
     with SingleTickerProviderStateMixin {
-  GlobalKey textKey = GlobalKey();
-  GlobalKey selfKey = GlobalKey();
-  double item_width = 0.0;
-  double self_width = 0.0;
-  final padding = 5.0;
-  final image_width = 30.0;
-//  var textPadding = 0.0;
-  List<Widget> views = [];
+  ScrollController _scrollController;
+  Timer _timer;
+  double _offset = 0.0;
+  int _itemCount = 100;
 
-  AnimationController controller;
-  Animation curve;
-  Animation<Offset> animation;
-  double animationValue;
-  bool animationComplate;
+  GlobalKey _textKey = GlobalKey();
+  GlobalKey _selfKey = GlobalKey();
+  double _item_width = 0.0;
+  double _self_width = 0.0;
+
+  /// 设置内容边距
+  final _padding = 5.0;
+  final _image_width = 30.0;
+
+  List<Widget> _views = [];
+  bool _flag = false;
+
+  AnimationController _controller;
+  Animation _curve;
+  Animation<Offset> _animation;
 
   @override
   void initState() {
@@ -41,6 +58,7 @@ class _NoticeBarState extends State<NoticeBar>
     super.initState();
     addAnimation();
     reloadSubviews();
+
     getMaxWidth();
   }
 
@@ -48,132 +66,170 @@ class _NoticeBarState extends State<NoticeBar>
   void didUpdateWidget(NoticeBar oldWidget) {
     // TODO: implement didUpdateWidget
     super.didUpdateWidget(oldWidget);
-    addAnimation();
-    reloadSubviews();
     getMaxWidth();
   }
 
   void getMaxWidth() {
     WidgetsBinding binding = WidgetsBinding.instance;
     binding.addPostFrameCallback((timeStamp) {
-      RenderObject renderObject = textKey.currentContext.findRenderObject();
-      RenderObject selfObject = selfKey.currentContext.findRenderObject();
-      setState(() {
-        item_width = renderObject.semanticBounds.size.width;
-        self_width = selfObject.semanticBounds.size.width;
-      });
+      RenderObject selfObject = _selfKey.currentContext.findRenderObject();
+      RenderObject renderObject = _textKey.currentContext.findRenderObject();
+      _self_width = selfObject.semanticBounds.size.width;
+      _item_width = renderObject.semanticBounds.size.width;
+
+      _flag = _item_width >
+          (_self_width -
+              (widget.leftWidget != null ? _image_width : 0) -
+              (widget.rightWidget != null ? _image_width : 0));
+      reloadSubviews();
     });
   }
 
   void reloadSubviews() {
     List<Widget> widgets = [];
     widgets.add(
-      item_width >
-          (self_width -
-              (widget.leftImageName != null ? image_width : 0) -
-              (widget.rightImageName != null ? image_width : 0))
-          ? SlideTransition(
-        transformHitTests: true,
-        textDirection: TextDirection.rtl,
-        position: this.animation,
-        child: Padding(
-          padding: EdgeInsets.only(left: animationComplate == true ?  100 : image_width),
-          child: Text(
-            widget.text,
-            key: textKey,
-            style: TextStyle(color: Colors.black),
-            maxLines: 1,
-          ),
-        ),
-      )
+      _flag
+          ? widget.type == NoticeType.subsection
+              ? SlideTransition(
+                  position: _animation,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: _image_width),
+                    child: Text(
+                      widget.text,
+                      key: _textKey,
+                      style: TextStyle(color: Colors.black),
+                      maxLines: 1,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+          scrollDirection: Axis.horizontal,
+          controller: _scrollController,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: _itemCount,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 44,
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: _image_width),
+                child: Text(
+                  widget.text,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    backgroundColor: Colors.red,
+                  ),
+                  maxLines: 1,
+                ),
+              ),
+            );
+          })
           : Positioned(
-        left: widget.leftImageName != null ? image_width : padding * 2,
-        child: Text(
-          widget.text,
-          key: textKey,
-          style: TextStyle(color: Colors.black),
-          maxLines: 1,
-        ),
-      ),
+              left: widget.leftWidget != null ? _image_width : _padding * 2,
+              child: Text(
+                widget.text,
+                key: _textKey,
+                style: TextStyle(color: Colors.black),
+                maxLines: 1,
+              ),
+            ),
     );
-    if (widget.leftImageName != null) {
+    if (widget.leftWidget != null) {
       widgets.add(Positioned(
         left: 0,
         top: 0,
         bottom: 0,
-        width: image_width,
+        width: _image_width,
         child: Container(
-          color: Colors.white,
-          child: Image(image: AssetImage(widget.leftImageName)),
+          color: widget.backgroundColor,
+          child: widget.leftWidget,
         ),
       ));
     }
-    if (widget.rightImageName != null) {
+    if (widget.rightWidget != null) {
       widgets.add(Positioned(
         right: 0,
         top: 0,
         bottom: 0,
-        width: image_width,
+        width: _image_width,
         child: Container(
-          color: Colors.white,
-          child: Image(
-            image: AssetImage(widget.rightImageName),
-          ),
+          color: widget.backgroundColor,
+          child: widget.rightWidget,
         ),
       ));
     }
 
     setState(() {
-      views = widgets;
+      _views = widgets;
     });
   }
 
   void addAnimation() {
-    controller = AnimationController(
-      duration: Duration(milliseconds: 10000),
-      vsync: this,
-    );
-    curve = CurvedAnimation(parent: controller, curve: Curves.linear);
-    animation = Tween(
-      begin: Offset(0.0, 0.0),
-      end: Offset(1.0, 0.0),
-    ).animate(curve)
-      ..addListener(() {
-        setState(() {
-          animationValue = animation.value.dx;
-          print(animationValue);
+    if (widget.type == NoticeType.subsection) {
+      _controller = AnimationController(
+        duration: Duration(milliseconds: 10000),
+        vsync: this,
+      );
+      _curve = CurvedAnimation(parent: _controller, curve: Curves.linear);
+      _animation = Tween(
+        begin: Offset(0.0, 0.0),
+        end: Offset(-1.0, 0.0),
+      ).animate(_curve)
+        ..addListener(() {
+          setState(() {});
+        })
+        ..addStatusListener((status) {
+          if (status == AnimationStatus.completed) {
+          } else if (status == AnimationStatus.dismissed) {}
         });
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
+      _controller.repeat();
+    } else {
+      _scrollController = ScrollController(
+        initialScrollOffset: _offset,
+      );
+      _scrollController.addListener(() {
+        if (_scrollController.position.maxScrollExtent == _scrollController.offset) {
+//          _scrollController.jumpTo(10.0);
         }
       });
-    controller.forward();
+      _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+        double newOffset = _scrollController.offset + 10;
+        if (newOffset != _offset) {
+          _offset = newOffset;
+          _scrollController.animateTo(_offset,
+              duration: Duration(milliseconds: 100), curve: Curves.linear);
+        }
+      });
+    }
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
-    controller.dispose();
+    if (_controller != null) {
+      _controller.dispose();
+    }
+    if (_timer != null) {
+      _timer.cancel();
+    }
+    if (_scrollController != null) {
+      _scrollController.dispose();
+    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-//    print('self_width==$self_width');
-//    print('item_width==$item_width');
     return Container(
-      key: selfKey,
+      key: _selfKey,
       height: 44,
       color: widget.backgroundColor,
       child: Stack(
+        overflow: Overflow.visible,
         alignment: Alignment.centerLeft,
-        children: views,
+        children: _views,
       ),
     );
   }
 }
-
