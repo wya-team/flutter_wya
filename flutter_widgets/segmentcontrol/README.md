6af52745ade34fa23da3f99d209adc184791d42f
李俊恒 <ljh@weiyian.com>
2020-03-30 15:53:55 +0800

    完善SegmentControl文档以及示例

--- flutter_widgets/segmentcontrol/README.md
+++ flutter_widgets/segmentcontrol/README.md
@@ -1,14 +1,68 @@
 # segmentcontrol
 
-A new Flutter package.
+### API文档
 
-## Getting Started
+##### 组件成员变量解释：
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+```dart
+  /// 需要展示的内容例如：
+  /// Map children = {'apple':Text('apple), 'orange':Text(orange)}
+  final Map<T, Widget> children;
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+  /// 默认选中哪一个数据传入children中的key值即可
+  final T groupValue;
+
+  /// 数据改变
+  final ValueChanged<T> onValueChanged;
+
+  /// 未选中颜色
+  final Color unselectedColor;
+
+  /// 选中颜色
+  final Color selectedColor;
+
+  /// 边框颜色
+  final Color borderColor;
+
+  /// 点击颜色
+  final Color pressedColor;
+
+  final EdgeInsetsGeometry padding;
+
+  final double width;
+
+  final double height;
+```
+
+##### Demo
+
+```dart
+  _getSegmentControl() {
+    Map<String, Text> map = {
+      'apple': Text('Apple'),
+      'orange': Text('Orange'),
+      'banana': Text('Banana')
+    };
+
+    String _fruit = 'apple';
+
+    return SegmentControl(
+      children: map,
+      groupValue: _fruit,
+      onValueChanged: (value) {
+        setState(() {
+          _fruit = value;
+        });
+      },
+      width: double.infinity,
+      height: 120,
+      unselectedColor: CupertinoColors.white,
+      // 未选中颜色
+      selectedColor: CupertinoColors.activeBlue,
+      // 选中颜色
+      borderColor: CupertinoColors.activeBlue,
+      // 边框颜色
+      pressedColor: const Color(0x33007AFF),
+    ); // 点击时候的颜色
+  }
+```
--- flutter_widgets/segmentcontrol/lib/segmentcontrol.dart
+++ flutter_widgets/segmentcontrol/lib/segmentcontrol.dart
@@ -1,7 +1,80 @@
 library segmentcontrol;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SegmentControl<T> extends StatefulWidget {
+  /// 需要展示的内容例如：Map children = {'apple':Text('apple), 'orange':Text(orange)}
+  final Map<T, Widget> children;
+
+  /// 默认选中哪一个数据传入children中的key值即可
+  final T groupValue;
+
+  /// 数据改变
+  final ValueChanged<T> onValueChanged;
+
+  /// 未选中颜色
+  final Color unselectedColor;
+
+  /// 选中颜色
+  final Color selectedColor;
+
+  /// 边框颜色
+  final Color borderColor;
+
+  /// 点击颜色
+  final Color pressedColor;
+
+  final EdgeInsetsGeometry padding;
+
+  final double width;
+
+  final double height;
+
+  SegmentControl({
+    Key key,
+    @required this.children,
+    @required this.onValueChanged,
+    @required this.width,
+    @required this.height,
+    this.groupValue,
+    this.unselectedColor,
+    this.selectedColor,
+    this.borderColor,
+    this.pressedColor,
+    this.padding,
+  })  : assert(children != null),
+        assert(children.length >= 2),
+        assert(onValueChanged != null),
+        assert(width != null),
+        assert(height != null),
+        assert(
+          groupValue == null ||
+              children.keys.any((T child) => child == groupValue),
+          'The groupValue must be either null or one of the keys in the children map.',
+        ),
+        super(key: key);
+
+  @override
+  _SegmentControlState createState() => _SegmentControlState();
+}
+
+class _SegmentControlState extends State<SegmentControl> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      child: CupertinoSegmentedControl(
+        children: widget.children,
+        groupValue: widget.groupValue,
+        onValueChanged: widget.onValueChanged,
+        unselectedColor: widget.unselectedColor,
+        selectedColor: widget.selectedColor,
+        pressedColor: widget.pressedColor,
+        borderColor: widget.borderColor,
+        padding: widget.padding,
+      ),
+    );
+  }
 }
