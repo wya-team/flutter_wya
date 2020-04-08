3afa5fcfb599641eb5c4da1e7ad61ce21b001fc6
毛春江 <mcj@weiyian.com>
2020-04-08 10:15:51 +0800

    录音完成

--- flutter_hardware/native_record/test/native_record_test.dart
+++ flutter_hardware/native_record/test/native_record_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:native_record/native_record.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('native_record');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await NativeRecord.platformVersion, '42');
   });
 }
