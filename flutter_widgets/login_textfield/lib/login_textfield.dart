0f3d91206a1b2e718d01c4c01aed4bb230809ed3
李俊恒 <ljh@weiyian.com>
2020-05-08 14:05:29 +0800

    更新searchBar

--- flutter_widgets/login_textfield/lib/login_textfield.dart
+++ flutter_widgets/login_textfield/lib/login_textfield.dart
@@ -1,7 +1,19 @@
 library login_textfield;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// 手机号输入框
+class AccountTextFormField extends StatefulWidget {
+  @override
+  _AccountTextFormFieldState createState() => _AccountTextFormFieldState();
+}
+
+class _AccountTextFormFieldState extends State<AccountTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextField(
+
+      ),
+    );
+  }
 }
