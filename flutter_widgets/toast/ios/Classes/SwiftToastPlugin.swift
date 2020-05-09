48f1593a78a8eaffbe3435d73190bacc718663a4
李世航 <1228506851@qq.com>
2020-05-09 16:29:55 +0800

    添加返回值

--- flutter_widgets/toast/ios/Classes/SwiftToastPlugin.swift
+++ flutter_widgets/toast/ios/Classes/SwiftToastPlugin.swift
@@ -21,6 +21,7 @@ public class SwiftToastPlugin: NSObject, FlutterPlugin {
                                        backgroundColor: UIColor(red: CGFloat(colorList[0].intValue)/255.0, green: CGFloat(colorList[1].intValue)/255.0, blue: CGFloat(colorList[2].intValue)/255.0, alpha: CGFloat(colorList[3].floatValue)),
                                        textColor: UIColor(red: CGFloat(textColorList[0].intValue)/255.0, green: CGFloat(textColorList[1].intValue)/255.0, blue: CGFloat(textColorList[2].intValue)/255.0, alpha: CGFloat(textColorList[3].floatValue)),
                                        bgViewUserInteractionUse: map["iosTouchBgDismiss"] as! Bool)
+            result(true)
         } else if map["gravity"] as! String == "bottom" {
             let colorList = map["bgcolor"] as! [NSNumber]
             let textColorList = map["textcolor"] as! [NSNumber]
@@ -30,6 +31,7 @@ public class SwiftToastPlugin: NSObject, FlutterPlugin {
             backgroundColor: UIColor(red: CGFloat(colorList[0].intValue)/255.0, green: CGFloat(colorList[1].intValue)/255.0, blue: CGFloat(colorList[2].intValue)/255.0, alpha: CGFloat(colorList[3].floatValue)),
             textColor: UIColor(red: CGFloat(textColorList[0].intValue)/255.0, green: CGFloat(textColorList[1].intValue)/255.0, blue: CGFloat(textColorList[2].intValue)/255.0, alpha: CGFloat(textColorList[3].floatValue)),
             bgViewUserInteractionUse: map["iosTouchBgDismiss"] as! Bool)
+            result(true)
         } else {
             result(false)
         }
@@ -39,17 +41,22 @@ public class SwiftToastPlugin: NSObject, FlutterPlugin {
         let stateCode = (map["status"] as! NSNumber).intValue
         if  stateCode == 1 {
             UIView.wya_successToast(withMessage: map["msg"] as! String)
+            result(true)
         } else if  stateCode == 0 {
             UIView.wya_toast(withMessage: map["msg"] as! String, imageString: "spin", autoRotation: true, imageType: WYAToastImageType.SVG, autoDismiss: map["canceledOnTouchOutside"] as! Bool, bgViewUserInteractionUse: true)
+            result(true)
         } else if  stateCode == -1 {
             UIView.wya_failToast(withMessage: map["msg"] as! String)
+            result(true)
         } else if  stateCode == -2 {
             UIView.wya_warningToast(withMessage: map["msg"] as! String)
+            result(true)
         } else {
             result(false)
         }
     } else if call.method == "cancelLoading" {
         UIView.wya_dismissToast()
+        result(true)
     }
   }
 }
