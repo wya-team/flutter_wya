5b628b09c3015b43b21684ef743a7b1d9cf61788
李世航 <1228506851@qq.com>
2020-05-15 15:01:46 +0800

    修复noticebar报错的问题

--- flutter_widgets/noticebar/lib/noticebar.dart
+++ flutter_widgets/noticebar/lib/noticebar.dart
@@ -1,4 +1,5 @@
 library noticebar;
+
 import 'dart:ui';
 import 'dart:async';
 
@@ -20,6 +21,7 @@ class NoticeBar extends StatefulWidget {
   Widget rightWidget;
   List<String> textList;
   Color backgroundColor;
+
   @override
   _NoticeBarState createState() => _NoticeBarState();
 }
@@ -40,43 +42,47 @@ class _NoticeBarState extends State<NoticeBar>
     super.initState();
     addAnimation();
     reloadSubviews();
-
   }
 
   @override
   void didUpdateWidget(NoticeBar oldWidget) {
     // TODO: implement didUpdateWidget
     super.didUpdateWidget(oldWidget);
+    addAnimation();
+    reloadSubviews();
   }
 
   void reloadSubviews() {
     List<Widget> widgets = [];
-    if(widget.textList.length > 1) {
-      widgets.add(ListView.builder(
-        reverse: widget.reverse,
+
+      List<Widget> aa = [];
+      widget.textList.forEach((element) {
+        aa.add(Container(
+          height: 44,
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: _image_width),
+            child: Text(
+              element,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                backgroundColor: widget.backgroundColor,
+              ),
+              maxLines: 1,
+            ),
+          ),
+        ));
+      });
+      widgets.add(
+        ListView(
+          reverse: widget.reverse,
           scrollDirection: widget.scrollDirection,
           controller: _scrollController,
           physics: NeverScrollableScrollPhysics(),
-          itemCount: widget.textList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 44,
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: _image_width),
-                child: Text(
-                  widget.textList[index],
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    backgroundColor: widget.backgroundColor,
-                  ),
-                  maxLines: 1,
-                ),
-              ),
-            );
-          }));
-    }
+          children: aa,
+        ),
+      );
 
     if (widget.leftWidget != null) {
       widgets.add(Positioned(
@@ -109,53 +115,46 @@ class _NoticeBarState extends State<NoticeBar>
   }
 
   void addAnimation() {
-
-      _scrollController = ScrollController(
-        initialScrollOffset: _offset,
-      );
-      _scrollController.addListener(() {
+    _scrollController = ScrollController(
+      initialScrollOffset: _offset,
+    );
+    _scrollController.addListener(() {
 //        print('_scrollController.position.maxScrollExtent==${_scrollController.position.maxScrollExtent}');
 //        print('_scrollController.offset==${_scrollController.offset}');
-        if (widget.scrollDirection == Axis.horizontal){
-          double offsetScroll = 0.0;
-          if (widget.leftWidget != null && widget.rightWidget != null){
-            offsetScroll = _scrollController.position.maxScrollExtent + _image_width * 2;
-          } else if (widget.leftWidget != null || widget.rightWidget != null){
-            offsetScroll = _scrollController.position.maxScrollExtent + _image_width;
-          } else {
-            offsetScroll = _scrollController.position.maxScrollExtent;
-          }
-          if (offsetScroll <= _scrollController.offset) {
-            _scrollController.jumpTo(0.0);
-          }
-        } else {
-          if (_scrollController.position.maxScrollExtent == _scrollController.offset) {
-            _scrollController.jumpTo(0.0);
-          }
+      if (widget.scrollDirection == Axis.horizontal) {
+        double offsetScroll = _scrollController.position.maxScrollExtent;
+        if (offsetScroll <= _scrollController.offset) {
+          _scrollController.jumpTo(0.0);
         }
-
-      });
-      if(widget.scrollDirection == Axis.horizontal) {
-        _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
-          double newOffset = _scrollController.offset + 10;
-          if (newOffset != _offset) {
-            _offset = newOffset;
-            _scrollController.animateTo(_offset,
-                duration: Duration(milliseconds: 100), curve: Curves.linear);
-          }
-        });
       } else {
-        _timer = Timer.periodic(Duration(seconds: 2), (timer) {
-          double newOffset = _scrollController.offset + 44;
-          if (newOffset != _offset) {
-            _offset = newOffset;
-            _scrollController.animateTo(_offset,
-                duration: Duration(milliseconds: 500), curve: Curves.linear);
-          }
-        });
+        if (_scrollController.position.maxScrollExtent ==
+            _scrollController.offset) {
+          _scrollController.jumpTo(0.0);
+        }
       }
-
-
+    });
+    if (widget.scrollDirection == Axis.horizontal) {
+      if (widget.textList.length == 1 && widget.textList.first.length < 20)
+        return;
+      _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+        double newOffset = _scrollController.offset + 10;
+        if (newOffset != _offset) {
+          _offset = newOffset;
+          _scrollController.animateTo(_offset,
+              duration: Duration(milliseconds: 100), curve: Curves.linear);
+        }
+      });
+    } else {
+      if (widget.textList.length < 2) return;
+      _timer = Timer.periodic(Duration(seconds: 2), (timer) {
+        double newOffset = _scrollController.offset + 44;
+        if (newOffset != _offset) {
+          _offset = newOffset;
+          _scrollController.animateTo(_offset,
+              duration: Duration(milliseconds: 500), curve: Curves.linear);
+        }
+      });
+    }
   }
 
   @override
